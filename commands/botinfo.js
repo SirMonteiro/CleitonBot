d78e29f11656122c8c0852f44c610d1a0768eee2
SirMonteiro <sm.gabriel007@gmail.com>
2020-06-24 09:21:38 -0300

    Update botinfo command

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -10,20 +10,20 @@ exports.run = async (client, message, args) => {
   let botembed = new RichEmbed()
     .setColor(rgb.hexrgb())
     .setThumbnail(bicon)
-    .addField("Nome do Bot", `:robot: ${client.user.username}`, inline)
+    .addField("Nome do Bot", `:robot: [${client.user.username}](https://cleitonbot.sirmonteiro.ga/)`, inline)
     //.addField("Dono do Bot", ":detective: <@345954606544846852>", inline)
-    .addField("Hospedado com o:", ":file_cabinet: [Fosshost](https://fosshost.org/)", inline)
-    .addField("Servidores", `🛡 ${servsize}`, inline)
-    .addField("Canais", `📁 ${chansize}`, inline)
-    .addField("Usuários", `:person_walking: ${usersize}`, inline)
+    .addField("Hospedado com o", ":file_cabinet: [Fosshost](https://fosshost.org/)", inline)
     .addField(
       "Biblioteca do Bot",
-      ":notebook_with_decorative_cover: Discord.js",
+      ":notebook_with_decorative_cover: [Discord.js](https://discord.js.org/)",
       inline
     )
+    .addField("Usuários", `:person_walking: ${usersize}`, inline)
+    .addField("Servidores", `🛡 ${servsize}`, inline)
+    .addField("Canais", `📁 ${chansize}`, inline)
     .addField("Criado em", "16/05/2020 15:36, horário de brasília")
     .setFooter(
-      `Informações sobre: ${client.user.username}. Criado por: SirMonteiro, Vonger, Lidac`
+      `Informações sobre: ${client.user.username}. Criado por: SirMonteiro#7498, Vonger#3958, Pão!?#3696`
     )
 
   message.channel.send(botembed);
